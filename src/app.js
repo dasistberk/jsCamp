e1e40f6368976d1062bdf2b0132386a6ad25184d
dasistberk <abay.berk20@gmail.com>
2021-10-19 00:02:39 +0300

    18 ekim ders sonu

---
+++ src/app.js
@@ -0,0 +1,23 @@
+console.log("Mervvvv <3")
+
+let dolarDun = 9.20
+let dolarBugun = 9.30
+
+{
+    let dolarDun = 9.10
+}
+
+console.log(dolarDun)
+
+const euroDun = 11.2
+//euroDun = 11 
+
+console.log(euroDun)
+
+//array
+let konutKredileri = ["Konut kredisi" , "Emlak kredisi" , "Taşıt kredisi" , "Kamu konut kredisi"]
+
+for (let i = 0; i < 3; i++) {
+    console.log();
+    
+}
